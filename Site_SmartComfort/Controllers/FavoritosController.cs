ef5f0249e1632779d4ae132aa31450b1e8082ea4
joao-lucas04 <joaolucas.anjospinto@gmail.com>
2024-05-25 17:39:45 -0300

    projeto

---
+++ Site_SmartComfort/Controllers/FavoritosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site_SmartComfort.Controllers
+{
+    public class FavoritosController : Controller
+    {
+        public IActionResult Favoritos()
+        {//retornando o repositorio com metodo todosClientes
+            return View();
+        }
+    }
+}
